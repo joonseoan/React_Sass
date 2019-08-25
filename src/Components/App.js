17087453589f06c8d8fa5e07289e18f991792836
Joon <joonseo.an@outlook.com>
2019-08-25 19:19:30 -0400

    initialized react sass

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+export default () => {
+    return (
+        <header className="header">
+            <div className="header__logo-box">
+                <img src="/img/logo-white.png" alt="Logo" className="header__logo" />
+            </div>
+            <div className="header__text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary--main">Outdoors</span>
+                    <span className="heading-primary--sub">is where life happens</span>
+                </h1>
+                <a href="#" className="btn btn--white btn--animated">Discover our tours</a>
+            </div>
+        </header>    
+    );
+}
