c90644b6befe5b99607bf754fd1a55c0666d2502
Joon <joonseo.an@outlook.com>
2019-08-29 23:50:07 -0400

    float based grid

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import './App.scss';
+import React, { Fragment } from 'react';
+
+import { Header, Section } from './Common';
+
 export default () => {
     return (
-        <header className="header">
-            <div className="header__logo-box">
-                <img src="/img/logo-white.png" alt="Logo" className="header__logo" />
-            </div>
-            <div className="header__text-box">
-                <h1 className="heading-primary">
-                    <span className="heading-primary--main">Outdoors</span>
-                    <span className="heading-primary--sub">is where life happens</span>
-                </h1>
-                <a href="#" className="btn btn--white btn--animated">Discover our tours</a>
-            </div>
-        </header>    
+        <Fragment>
+            <Header />
+            <Section />
+        </Fragment>
     );
 }
