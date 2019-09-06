b0b78c6cde10e78512ad0462733d022269462318
Joon <joonseo.an@outlook.com>
2019-09-05 22:46:01 -0400

    added background image style

--- src/Components/Common/Sections/SectionTours.js
+++ src/Components/Common/Sections/SectionTours.js
@@ -14,7 +14,18 @@ const SectionTours = props => {
                     <div className="card">
                         {/* Eelement */}
                         <div className="card__side card__side--front card__side--front-1">
-                            Front
+                            {/* card picture */}
+                            <div className="card__picture card__picture--1">
+                                &nbsp;
+                            </div>
+                            {/* card heading */}
+                            <div className="card__heading">
+                                Heading
+                            </div>
+                            {/* card details */}
+                            <div className="card_details">
+                                details
+                            </div>
                         </div>
                         <div className="card__side card__side--back card__side--back-1">
                             Back
