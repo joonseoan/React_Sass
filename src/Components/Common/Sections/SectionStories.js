16841572e1b0ffa7b37fc9ba196fae8804ededf9
Joon <joonseo.an@outlook.com>
2019-10-15 23:17:22 -0400

    finished media query

--- src/Components/Common/Sections/SectionStories.js
+++ src/Components/Common/Sections/SectionStories.js
@@ -28,19 +28,20 @@ const SectionStories = props => {
                         <p>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
                     </div>
                 </div>
+            </div>
             <div className="row">
                 <div className="story">
-                        <figure className="story__shape">
-                            <img src="/img/nat-9.jpg" alt="person on a tour" className="story__img" />
-                            <figcaption className="story__caption">Michael Jose</figcaption>
-                        </figure>
-                        <div className="story__text">
-                            <h3 className="heading-tertiary u-margin-bottom-small">Wow My Life is Completely Different</h3>
-                            <p>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-                        </div>
+                    <figure className="story__shape">
+                        <img src="/img/nat-9.jpg" alt="person on a tour" className="story__img" />
+                        <figcaption className="story__caption">Michael Jose</figcaption>
+                    </figure>
+                    <div className="story__text">
+                        <h3 className="heading-tertiary u-margin-bottom-small">Wow My Life is Completely Different</h3>
+                        <p>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
                     </div>
                 </div>
             </div>
+            
             <div className="u-center-text u-margin-top-huge">
                 <a href="#" className="btn-text">Read all stories &rarr;</a>
             </div>
